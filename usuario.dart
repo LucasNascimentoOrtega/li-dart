84209c9b85892eb5f45cc8b271cade30fd32bfb8
LucasNascimentoOrtega <lucasortega371@gmail.com>
2026-09-24 22:48:07 -0400

    final usuario . dart

--- usuario.dart
+++ usuario.dart
@@ -2,4 +2,9 @@ class Usuario {
   String nick;
   int idade;
   bool ativo = true;
+  Usuario({required this.nick, required this.idade});
+}
+
+class Moderador extends Usuario {
+  Moderador({required super.nick, required super.idade});
 }
