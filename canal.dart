e7049e716203f726ef34546bd271481777d63018
LucasNascimentoOrtega <lucasortega371@gmail.com>
2026-09-24 22:52:08 -0400

    final canal.dart

--- canal.dart
+++ canal.dart
@@ -6,3 +6,22 @@ class Mensagem {
 
   Mensagem(this.autor, this.texto);
 }
+
+class Canal {
+  final String nome;
+  final List<Mensagem> _mensagens = [];
+
+  Canal(this.nome);
+
+  void adicionarMensagem(Mensagem msg) {
+    _mensagens.add(msg);
+  }
+
+  void exibirMensagens() {
+    print('#$nome');
+
+    for (final msg in _mensagens) {
+      print('${msg.autor.nick}: ${msg.texto}');
+    }
+  }
+}
